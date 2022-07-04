c7dfc7b0b7088e573d33511c363c190ea277d573
Thiago Mendes <thiagomendes.dev@gmail.com>
2022-07-04 17:16:12 -0300

    Final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObjects";
+import {Pet} from '../models/pets';
+
 
 export const search = (req: Request, res: Response)=>{
+  let query: string = req.query.q as string;
 
+  if(!query){
+    res.redirect('/');
+    return;
+  }
+
+  let list = Pet.getFromName(query);
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    query
+  });
 }
